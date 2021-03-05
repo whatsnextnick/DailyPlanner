7f8d66d107f8b901f20106833d1bdd4142c385cc
Jawascript <n.jmonplaisir@gmail.com>
2021-03-05 04:46:58 -0500

    add date and card to timeblock

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,19 @@
-const moment = require("moment");
+$(document).ready(() => {
+  // const moment = require("moment");
+  // Create "current date" format
+  const date = moment().format("MM-DD-YYYY");
+  console.log(date);
 
-// Create "current date" format
-const date = moment().format("MM-DD-YYYY");
-console.log(date);
+  $("#currentDay").text(date);
 
-$("#currentDate").text(date);
+  $("#timeblock").append($("<div id ='timeCard' class = 'card-section'>"));
+//   .attr({id:"timeCard", class":"card-section"}));
+  // eslint-disable-next-line no-unused-vars
+  const workhours = 9;
+  
+//   displayTime() => {
+//     for(i = 0; i<workhours; i++) {
+//         $("#timeblock").append($());
+//     };
+// }
+});
