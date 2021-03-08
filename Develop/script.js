b796df53c7d35f14e09a1f96d46a1e755d7c1ba6
Jawascript <n.jmonplaisir@gmail.com>
2021-03-08 03:36:54 -0500

    adding save btn functionality

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,18 @@ $(document).ready(() => {
   // Create "current date" format
   const date = moment().format("MM-DD-YYYY");
   console.log(date);
-
   $("#currentDay").text(date);
+  
+  $("#save8").click(function() {
+    event.preventDefault();
 
-  $("#timeblock").append($("<div>").addClass("cardbody").text("Hi"));
+    var todo = $("#input8").val();
+    console.log(todo);
+  });
+
+
+
+//   $("#timeblock").append($("<div>").addClass("cardbody").text("Hi"));
 //   .attr({id:"timeCard", class":"card-section"}));
   // eslint-disable-next-line no-unused-vars
   const workhours = 9;
