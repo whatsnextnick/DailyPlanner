51d6e1ff41439cf7428d52a402a19719de6b9104
Jawascript <n.jmonplaisir@gmail.com>
2021-03-10 19:55:33 -0500

    resolved all apparant bugs. Localstorage value constant regardless of refresh

--- script.js
+++ script.js
@@ -14,58 +14,50 @@ $(document).ready(() => {
     console.log(workday);
 
     //Display localStorage data to frontend
-    $("#input8").attr("placeholder", workday[0]);
-    $("#input9").attr("placeholder", workday[1]);
-    $("#input10").attr("placeholder", workday[2]);
-    $("#input11").attr("placeholder", workday[3]);
-    $("#input12").attr("placeholder", workday[4]);
-    $("#input13").attr("placeholder", workday[5]);
-    $("#input14").attr("placeholder", workday[6]);
-    $("#input15").attr("placeholder", workday[7]);
-    $("#input16").attr("placeholder", workday[8]);
-    $("#input17").attr("placeholder", workday[9]);
-  
-    const currentTime = moment().hour();
-    console.log(currentTime);
-    console.log(parseInt($("#3").attr("id")));
-    
-    var hour8 = parseInt($("#8").attr("id"));
-    var hour9 = parseInt($("#9").attr("id"));
-    var hour10 = parseInt($("#10").attr("id"));
-    var hour11 = parseInt($("#11").attr("id"));
-    var hour12 = parseInt($("#12").attr("id"));
-    var hour13 = parseInt($("#13").attr("id"));
-    var hour14 = parseInt($("#14").attr("id"));
-    var hour15 = parseInt($("#15").attr("id"));
-    var hour16 = parseInt($("#16").attr("id"));
-    var hour17 = parseInt($("#17").attr("id"));
-  
-    var workhours = [hour8, hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
-  
-      //add the design based on blocks relation to current time
-    colorcord(workhours);
-    function colorcord(timearr){
-      for(i=0; i<workhours.length; i++){
-        if (timearr[i] === currentTime){
-          $("#"+ timearr[i].toString()).siblings(".input-group-text").addClass("present");
-        } else if (timearr[i] < currentTime){
-          $("#"+ timearr[i].toString()).siblings(".input-group-text").addClass("past");
-        } else if (timearr[i] > currentTime){
-          $("#"+ timearr[i].toString()).siblings(".input-group-text").addClass("future");
-        }
-      }
-    }
+    $("#input8").val(workday[0]);
+    $("#input9").val(workday[1]);
+    $("#input10").val(workday[2]);
+    $("#input11").val(workday[3]);
+    $("#input12").val(workday[4]);
+    $("#input13").val(workday[5]);
+    $("#input14").val(workday[6]);
+    $("#input15").val(workday[7]);
+    $("#input16").val(workday[8]);
+    $("#input17").val(workday[9]);
+
   };
+  //get the current time in a integer value of 1-24 to compare to timeblock
+  const currentTime = moment().hour();
+  console.log(currentTime);
+  console.log(parseInt($("#8").attr("id")));
+  
+  //get the id attribute for each timeblock and stores it as integer
+  var hour8 = parseInt($("#8").attr("id"));
+  var hour9 = parseInt($("#9").attr("id"));
+  var hour10 = parseInt($("#10").attr("id"));
+  var hour11 = parseInt($("#11").attr("id"));
+  var hour12 = parseInt($("#12").attr("id"));
+  var hour13 = parseInt($("#13").attr("id"));
+  var hour14 = parseInt($("#14").attr("id"));
+  var hour15 = parseInt($("#15").attr("id"));
+  var hour16 = parseInt($("#16").attr("id"));
+  var hour17 = parseInt($("#17").attr("id"));
 
-
-  // function colorcord(timearr){
-  //   for (i=0; i<workday; i++) {
-  //     if (timearr[i].val().isSame(date, "hour")){
-  //       timearr[i].addClass("present");
-  //     // } else if ()
-  //     };
-  //   };
-  // };
+  var workhours = [hour8, hour9, hour10, hour11, hour12, hour13, hour14, hour15, hour16, hour17];
+  
+  //add the design based on blocks relation to current time
+  colorcord(workhours);
+  function colorcord(timearr){
+    for(i=0; i<timearr.length; i++){
+      if (timearr[i] === currentTime){
+        $("#"+ timearr[i].toString()).siblings(".input-group-text").addClass("present");
+      } else if (timearr[i] < currentTime){
+        $("#"+ timearr[i].toString()).siblings(".input-group-text").addClass("past");
+      } else if (timearr[i] > currentTime){
+        $("#"+ timearr[i].toString()).siblings(".input-group-text").addClass("future");
+      };
+    };
+  };
 
   //Save each hour's "to-do" per save event
   $(".btn").click( function(){
